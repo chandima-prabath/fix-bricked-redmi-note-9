970b9d9316e088f336425355d70da50b6aad7457
Chandima Prabath <chandimaprbath.h.g@gmail.com>
2023-08-13 10:47:08 +0530

    del

--- bypass/src/logger.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-
-def log(string):
-    line = "[{}] {}".format(datetime.datetime.now(), string)
-    print(line)
-
-    with open("bypass_utility.log", "a") as out:
-        out.write(line + "\n")
